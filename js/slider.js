772817c3f4064df80a01b1f5dacd73bf1d32acaa
KaptonPlanet <sean.sweeney01@gmail.com>
2019-02-10 16:59:53 -0500

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    document.getElementById('test').className = 'slideDown';
+  }, 3000);
+}, false);
+
+
+})(jQuery);
